725bdb369ad09724e2c97e05386aac4ee8b7eb8e
Archi91 <aligator91>
2017-05-04 17:09:55 +0200

    Implement minor version of add_product template

---
+++ main.py
@@ -0,0 +1,37 @@
+from model.product import Product
+from flask import Flask, render_template, redirect, url_for, request, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/products/')
+def index():
+    products = Product.get_all_products()
+    return render_template('index.html', products=products)
+
+@app.route('/products/add_product', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form['description']
+        if request_validate(name, price, description):
+            Product.add_product(name, price, description)
+            return redirect(url_for('index'))
+
+    return render_template('add_product.html')
+
+def request_validate(name, price, description):
+    validator = True
+    if not name:
+        validator = False
+    if not price:
+        validator = False
+    if not description:
+        validator = False
+    return validator
+
+
+if __name__ == '__main__':
+    app.secret_key = "SECRET_KEY"
+    app.run(debug=True, host='0.0.0.0')
